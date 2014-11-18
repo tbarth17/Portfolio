1c519dc6575b08d79a3bcf2e794219faddf6a3d1
Tommy Barth <tommyhbrc@gmail.com>
2014-11-18 13:43:16 -0800

    Builds for deployment on godaddy

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,28 @@
+console.log("Welcome to my portfolio!")
   $(document).ready(function(){
     $(".nav-container").sticky({topSpacing:0});
   });
 
   $('.to-top').click(function() {
-    $('body').animate({
+    $('body, html').animate({
         scrollTop: $('.header').offset().top
     }, 800);
 });
 
   $('.to-work').click(function() {
-    $('body').animate({
+    $('body, html').animate({
         scrollTop: $('.work').offset().top - 60
     }, 800);
 });
 
   $('.to-skills').click(function() {
-    $('body').animate({
+    $('body, html').animate({
         scrollTop: $('.tool-kit').offset().top - 80
     }, 800);
 });
 
   $('.to-contact').click(function() {
-    $('body').animate({
+    $('body, html').animate({
         scrollTop: $('.contact').offset().top - 80
     }, 800);
 });
